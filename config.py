20eba3e2ee88660be8b2867a211cc26140722a78
spardon <574060769@qq.com>
2017-12-07 02:06:34 -0800

    fix

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 DEBUG = True
+
+# MYSQL 数据库配置信息
 SQLALCHEMY_DATABASE_URI = 'mysql://degreenplay:spd123456@db4free.net:3307/degreenplay'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
